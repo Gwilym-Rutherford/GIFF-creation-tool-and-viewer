db98c6772f6bd117e63f642ea6138c09ebc7f11a
Gwilym-Rutherford <58346802+Gwilym-Rutherford@users.noreply.github.com>
2024-09-08 18:32:22 +0100

    Add saves image data but not headers

--- createGIFF.js
+++ createGIFF.js
@@ -1,3 +1,16 @@
 export function saveFormat(imageData){
-    console.log(imageData.data);
+    let byteArray = new ArrayBuffer(imageData.data.length);
+    let typeArray = new Uint8Array(byteArray);
+    typeArray.set(imageData.data);
+
+    let blob = new Blob([byteArray], { type: "application/octet-stream" });
+    let blobURL =  URL.createObjectURL(blob);
+
+    let temp = document.createElement("a");
+    temp.href = blobURL;
+    temp.download = "giffTest.giff";
+    document.body.appendChild(temp);
+    temp.click();
+    URL.revokeObjectURL(blobURL);
+    document.body.removeChild(temp);
 }
