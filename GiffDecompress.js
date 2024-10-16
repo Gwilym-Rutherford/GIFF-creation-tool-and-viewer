f9f48655ce54a7cac7feb32dfddb49eb81132f37
Gwilym-Rutherford <58346802+Gwilym-Rutherford@users.noreply.github.com>
2024-10-16 16:49:16 +0100

    Remove useless logging

--- GiffDecompress.js
+++ GiffDecompress.js
@@ -11,8 +11,6 @@ export class GiffDecompress extends Giff{
             returnPixelData.push(...Array(length).fill(constructedHex));
         }
 
-        console.log(returnPixelData);
-
         return returnPixelData;
     }
 
